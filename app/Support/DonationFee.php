f4887c89528534d989fb26cb23cd4edd22aae279
BeiyaC <anis.grigah@le-campus-numerique.fr>
2023-05-03 16:39:34 +0200

    First iteration

--- app/Support/DonationFee.php
+++ app/Support/DonationFee.php
@@ -3,20 +3,64 @@
 namespace App\Support;
 
 
+use Exception;
+
 class DonationFee
 {
 
-    private $donation;
-    private $commissionPercentage;
+    private int $donation;
+    private int $commissionPercentage;
+    private const FIXED_FEE = 50;
 
     public function __construct(int $donation, int $commissionPercentage)
     {
-        $this->donation = $donation;
-        $this->commissionPercentage = $commissionPercentage;
+        $this->donation = $this->handleDonation($donation);
+        $this->commissionPercentage = $this->handleCommissionPercentage($commissionPercentage);
     }
 
-    public function getCommissionAmount()
+    public function getCommissionAmount(): int
     {
         return ($this->donation * $this->commissionPercentage)/100;
+    }
+
+    public function getAmountCollected(): int
+    {
+        return $this->donation -$this->getFixedAndCommissionFeeAmount();
+    }
+
+    public function getFixedAndCommissionFeeAmount(): int
+    {
+        $result = $this->getCommissionAmount() + self::FIXED_FEE;
+        if ($result > 500){
+            return 500;
+        }
+        return $result;
+    }
+
+    public function getSummary(): array
+    {
+        return [
+            'donation'=>$this->donation,
+            'commission'=>$this->getCommissionAmount(),
+            'fixedFee'=>self::FIXED_FEE,
+            'fixedAndCommission'=>$this->getFixedAndCommissionFeeAmount(),
+            'amountCollected'=>$this->getAmountCollected()
+        ];
+    }
+
+    private function handleDonation(int $donation): int
+    {
+        if ( $donation < 100) {
+            throw new Exception("Value should be superior as 100");
+        }
+        return $donation;
+    }
+
+    private function handleCommissionPercentage(int $commissionPercentage): int
+    {
+        if ($commissionPercentage < 0 || $commissionPercentage > 30) {
+            throw  new Exception("Value should be positive and less than 30");
+        }
+        return $commissionPercentage;
     }
 }
--- tests/Unit/DonationFeeTest.php
+++ tests/Unit/DonationFeeTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Support\DonationFee;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class DonationFeeTest extends TestCase
@@ -10,7 +12,7 @@ class DonationFeeTest extends TestCase
     public function test_commission_amount_is_10_cent_form_donation_of_100_cents_and_commission_of_10_percent()
     {
         // Etant donné une donation de 100 et commission de 10%
-        $donationFees = new \App\Support\DonationFee(100, 10);
+        $donationFees = new DonationFee(100, 10);
 
         // Lorsque qu'on appel la méthode getCommissionAmount()
         $actual = $donationFees->getCommissionAmount();
@@ -23,13 +25,71 @@ class DonationFeeTest extends TestCase
     public function test_commission_amount_is_20_cents_form_donation_of_200_cents_and_commission_of_10_percent()
     {
         // Etant donné une donation de 200 et commission de 10%
-        $donationFees = new \App\Support\DonationFee(200, 10);
+        $donationFees = new DonationFee(200, 10);
 
         // Lorsque qu'on appel la méthode getCommissionAmount()
-        $actual = $donationFees->getCommissionlsAmount();
+        $actual = $donationFees->getCommissionAmount();
 
         // Alors la Valeur de la commission doit être de 20
         $expected = 20;
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_total_amount_collected()
+    {
+        $ownerAmount = new DonationFee(200, 10);
+        $actual = $ownerAmount->getAmountCollected();
+
+        $expected = 130;
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_commission_amount_is_not_between_0_and_30_percent()
+    {
+        $this->expectException(Exception::class);
+
+        $donationFee = new DonationFee(200, 40);
+
+    }
+
+    public function test_donation_is_not_greater_than_99()
+    {
+        $this->expectException(Exception::class);
+
+        $donationFee = new DonationFee(50, 10);
+
+    }
+
+    public function test_fixed_fee_and_commission_amount()
+    {
+        $donationFee = new DonationFee(100, 10);
+        $actual = $donationFee->getFixedAndCommissionFeeAmount();
+
+        $expected = 60;
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_total_donation_fee_cannot_be_greater_than_50()
+    {
+        $donationFee = new DonationFee(6000, 20);
+        $actual = $donationFee->getFixedAndCommissionFeeAmount();
+
+        $expected = 500;
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_donation_summary()
+    {
+        $donationFee = new DonationFee(6000, 20);
+        $actual = $donationFee->getSummary();
+
+        $expected = [
+            'donation'=>6000,
+            'commission'=>1200,
+            'fixedFee'=>50,
+            'fixedAndCommission'=>500,
+            'amountCollected'=>5500
+        ];
         $this->assertEquals($expected, $actual);
     }
 }
